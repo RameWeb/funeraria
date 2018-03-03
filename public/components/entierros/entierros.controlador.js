644d17f2d071f4e26b0873803512c64ed0f9641f
Jason Gutierrez <jason.gutierrezp@gmail.com>
2018-03-03 11:31:10 -0600

    Ajustes en controlador entierros
    Ajuste y verificacion de funcionalidad probadas en console y localStorage temporal.

--- public/components/entierros/entierros.controlador.js
+++ public/components/entierros/entierros.controlador.js
@@ -37,35 +37,21 @@
     
     // Funcion que es llamda desde el html para regustra un nuevo usuario
     vm.registrarEntierro = (pnuevoEntierro) => {
-      // let prioridades = vm.listarPrioridades(vm.nuevoEntierro.p1,vm.nuevoEntierro.p2,vm.nuevoEntierro.p3,vm.nuevoEntierro.p4);
       vm.nuevoEntierro.prioridad = vm.listarPrioridades(vm.nuevoEntierro.p1,vm.nuevoEntierro.p2,vm.nuevoEntierro.p3,vm.nuevoEntierro.p4);
-      // console.log(vm.nuevoEntierro.prioridad);
 
       // Tomamos el objeto sin formato y lo comvertimos en un objeto de tipo cliente
       let objNuevoEntierro = new Entierro(vm.nuevoEntierro.horaInicio,vm.nuevoEntierro.horaFinal,vm.nuevoEntierro.fecha,vm.nuevoEntierro.lugar, vm.nuevoEntierro.prioridad);
-      localStorage.setItem('nuevoEntierro', JSON.stringify(objNuevoEntierro));
-      console.log(objNuevoEntierro);
-
-      // console.log('objeto sin formato');
-      // console.log(pnuevoEntierro);
-
-      // console.log('---------')
-
-      // console.log('objeto con formato');
       // console.log(objNuevoEntierro);
+      // localStorage.setItem('nuevoEntierro', JSON.stringify(objNuevoEntierro));
       // console.log(objNuevoEntierro.obtenerHora()); //metodo de horas y minutos
 
+      // Retroalimentacion Visual para los usuarios: SweetAlert
       swal("Registro exitoso", "El entierro se ha sido registrado correctamente", "success", {
         button: "Aceptar",
       });
 
       // Pasamos al servicio el nuevo obj de tipo cliente para ser almacenado en el localStorage
       // servicioEntierros.agregarEntierro(pnuevoEntierro)
-
-      // Retroalimentacion Visual para los usuarios: SweetAlert
-      // swal("Registro exitoso", "El usuario ha sido registrado correctamente", "success", {
-      //   button: "Aceptar",
-      // });
 
       // Se limpia el formulario
       vm.nuevoEntierro = null;
