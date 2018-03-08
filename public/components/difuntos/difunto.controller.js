dba69f12b5c388ade305ad5074682f740665f693
Christine <cvarelaf@ucenfotec.ac.cr>
2018-03-08 13:05:53 -0600

    Difuntos
    Fix

--- public/components/difuntos/difunto.controller.js
+++ public/components/difuntos/difunto.controller.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
   angular
-  .module('arquitectura')
+  .module('labFuneraria')
   .controller('controladorDifunto', controladorDifunto);
 
   controladorDifunto.$inject = ['$stateParams', '$state', 'servicioUsuarios']
