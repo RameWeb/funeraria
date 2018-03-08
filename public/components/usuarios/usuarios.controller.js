b02ded89ea9524374a8f668547c7ca9db146a019
Christine <cvarelaf@ucenfotec.ac.cr>
2018-03-08 11:52:00 -0600

    Usuarios
    Errores

--- public/components/usuarios/usuarios.controller.js
+++ public/components/usuarios/usuarios.controller.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
   angular
-  .module('arquitectura')
+  .module('labFuneraria')
   .controller('controladorUsuarios', controladorUsuarios);
 
   controladorUsuarios.$inject = ['$stateParams', '$state', 'servicioUsuarios'];
