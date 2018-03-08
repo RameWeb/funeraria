f8a43e8b6c4c05d1c39962b80af890838702e136
Christine <cvarelaf@ucenfotec.ac.cr>
2018-03-08 12:44:13 -0600

    Error
    Fix

--- public/components/usuarios/usuarios.controller.js
+++ public/components/usuarios/usuarios.controller.js
@@ -7,20 +7,20 @@
   controladorUsuarios.$inject = ['$stateParams', '$state', 'servicioUsuarios'];
 
   function controladorUsuarios($stateParams, $state, servicioUsuarios){
-
     let vm = this;
 
     vm.nuevoUsuario = {};
     vm.listaUsuarios = listarUsuarios();
 
     listarUsuarios();
-    // FunciÃ³n que es llamada desde el html para registra un nuevo usuario
+    // Función que es llamda desde el html para registra un nuevo usuario
     vm.registrarUsuario = (pnuevoUsuario) => {
 
       console.log(pnuevoUsuario);
 
-      // Tomamos el objeto sin formato y lo convertimos en una instancia de la clase cliente
+      // Tomamos el objeto sin formato y lo comvertimos en una instancia de la clase cliente
       let objNuevoUsuario = new Cliente(pnuevoUsuario.foto,pnuevoUsuario.cedula,pnuevoUsuario.nombre,pnuevoUsuario.apellido,pnuevoUsuario.fechanacimiento,pnuevoUsuario.sexo,pnuevoUsuario.ubicacion,pnuevoUsuario.provincia,pnuevoUsuario.canton,pnuevoUsuario.distrito,pnuevoUsuario.usuario,pnuevoUsuario.contrasenna);
+
      
       console.log('objeto con usuario');
       console.log(objNuevoUsuario);
@@ -28,7 +28,7 @@
       // Pasamos al servicio el nuevo obj de tipo cliente para ser almacenado en el localStorage
       servicioUsuarios.addUsuario(objNuevoUsuario);
 
-      // RetroalimentaciÃ³n Visual para los usuarios
+      // Retroalimentación Visual para los usuarios
       swal("Registro exitoso", "El usuario ha sido registrado correctamente", "success", {
         button: "Aceptar",
       });
@@ -47,6 +47,8 @@
     function listarUsuarios() {
       vm.listaUsuarios = servicioUsuarios.getUsuarios();
     }
+
+    
 
   }
 })();
