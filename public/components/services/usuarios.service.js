14302942e00a5c76fa965bb7f51268f8e4f5d65c
Christine <cvarelaf@ucenfotec.ac.cr>
2018-03-08 13:03:24 -0600

    Errors
    Fix

--- public/components/services/usuarios.service.js
+++ public/components/services/usuarios.service.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
   angular
-  .module('arquitectura')
+  .module('labFuneraria')
   .service('servicioUsuarios', servicioUsuarios);
 
   servicioUsuarios.$inject = ['$log','$http'];
